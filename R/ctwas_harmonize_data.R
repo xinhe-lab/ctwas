f0b0c20a3f45cdfe0cc7bc0c3f6b03f21614d23a
simingz <michellezhao.student@gmail.com>
2021-04-22 11:22:02 -0500

    harmonization remove ambiguous strand

--- R/ctwas_harmonize_data.R
+++ R/ctwas_harmonize_data.R
@@ -1,34 +1,36 @@
 
 # flip alleles
-# Adapted from the allele_qc function
+# Copied from allele.qc function
 # in https://github.com/gusevlab/fusion_twas/blob/master/FUSION.assoc_test.R,
-# the difference is no snps are removed but in FUSION strand ambiguous SNPs
-# are removed
-flip_allele <- function(a1,a2,ref1,ref2) {
-  a1 <- toupper(a1)
-  a2 <- toupper(a2)
-  ref1 <- toupper(ref1)
-  ref2 <- toupper(ref2)
+allele.qc = function(a1,a2,ref1,ref2) {
+  a1 = toupper(a1)
+  a2 = toupper(a2)
+  ref1 = toupper(ref1)
+  ref2 = toupper(ref2)
 
-  ref <- ref1
-  flip <- ref
-  flip[ref == "A"] <- "T"
-  flip[ref == "T"] <- "A"
-  flip[ref == "G"] <- "C"
-  flip[ref == "C"] <- "G"
-  flip1 <- flip
+  ref = ref1
+  flip = ref
+  flip[ref == "A"] = "T"
+  flip[ref == "T"] = "A"
+  flip[ref == "G"] = "C"
+  flip[ref == "C"] = "G"
+  flip1 = flip
 
-  ref <- ref2
-  flip <- ref
-  flip[ref == "A"] <- "T"
-  flip[ref == "T"] <- "A"
-  flip[ref == "G"] <- "C"
-  flip[ref == "C"] <- "G"
-  flip2 <- flip
+  ref = ref2
+  flip = ref
+  flip[ref == "A"] = "T"
+  flip[ref == "T"] = "A"
+  flip[ref == "G"] = "C"
+  flip[ref == "C"] = "G"
+  flip2 = flip;
 
-  ifflip <- (a1 == ref2 & a2 == ref1) | (a1 == flip2 & a2 == flip1)
+  snp = list()
+  snp[["keep"]] = !((a1=="A" & a2=="T") | (a1=="T" & a2=="A") | (a1=="C" & a2=="G") | (a1=="G" & a2=="C"))
+  snp[["keep"]][ a1 != "A" & a1 != "T" & a1 != "G" & a1 != "C" ] = F
+  snp[["keep"]][ a2 != "A" & a2 != "T" & a2 != "G" & a2 != "C" ] = F
+  snp[["flip"]] = (a1 == ref2 & a2 == ref1) | (a1 == flip2 & a2 == flip1)
 
-  return(ifflip)
+  return(snp)
 }
 
 #' Harmonize z scores from GWAS to match ld reference genotypes.
@@ -45,17 +47,26 @@ harmonize_z_ld <- function(z_snp, ld_snpinfo){
     if (length(snpnames) != 0){
       z.idx <- match(snpnames, z_snp$id)
       ld.idx <- match(snpnames, ld_snpinfo$id)
-      ifflip <- flip_allele(z_snp[z.idx, ]$A1, z_snp[z.idx, ]$A2,
+      qc <- allele.qc(z_snp[z.idx, ]$A1, z_snp[z.idx, ]$A2,
                             ld_snpinfo[ld.idx, ]$alt, ld_snpinfo[ld.idx, ]$ref)
 
-      z_snp[z.idx, ][ifflip, c("A1", "A2")] <- z_snp[z.idx, ][ifflip, c("A2", "A1")]
-      z_snp[z.idx, ][ifflip, "z"] <- - z_snp[z.idx, ][ifflip, "z"]
+      ifflip <- qc[["flip"]]
+      ifremove <- !qc[["keep"]]
+
+      flip.idx <- z.idx[ifflip]
+      z_snp[flip.idx, c("A1", "A2")] <- z_snp[flip.idx, c("A2", "A1")]
+      z_snp[flip.idx, "z"] <- - z_snp[flip.idx, "z"]
+
+      remove.idx <- z.idx[ifremove]
+      if (length(remove.idx) != 0) {
+        z_snp <- z_snp[-remove.idx, ,drop = F]
+      }
     }
     return(z_snp)
 }
 
 #' Harmonize z scores from GWAS to match ld reference genotypes.
-#' Flip signs when reverse complement matches.
+#' Flip signs when reverse complement matches, remove strand ambiguous SNPs
 #' @param wgt.matrix from FUSION weight .Rdat file
 #' @param snps from FUSION weight .Rdat file
 #'  with columns "chrom", "id", "pos", "alt", "ref". The effect allele
@@ -71,11 +82,20 @@ harmonize_wgt_ld <- function(wgt.matrix, snps, ld_snpinfo){
   if (length(snpnames) != 0) {
     snps.idx <- match(snpnames, snps$id)
     ld.idx <- match(snpnames, ld_snpinfo$id)
-    ifflip <- flip_allele(snps[snps.idx, ]$alt, snps[snps.idx, ]$ref,
+    qc <- allele.qc(snps[snps.idx, ]$alt, snps[snps.idx, ]$ref,
                           ld_snpinfo[ld.idx, ]$alt, ld_snpinfo[ld.idx, ]$ref)
+    ifflip <- qc[["flip"]]
+    ifremove <- !qc[["keep"]]
+
     flip.idx <- snps.idx[ifflip]
     snps[flip.idx, c("alt", "ref")] <-  snps[flip.idx, c("ref", "alt")]
     wgt.matrix[flip.idx, ] <- - wgt.matrix[flip.idx, ]
+
+    remove.idx <- snps.idx[ifremove]
+    if (length(remove.idx) != 0) {
+      snps <- snps[-remove.idx, ,drop = F]
+      wgt.matrix <- wgt.matrix[-remove.idx, , drop = F]
+    }
   }
   return(list("wgt" = wgt.matrix, "snps" = snps))
 }
