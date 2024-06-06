ddc2a5235ce86c44eba8fada4416b01685e7780f
kevinlkx <kevinlkx@gmail.com>
2024-06-06 14:52:00 -0500

    added undefined functions from susie

--- R/susie_susie_utils.R
+++ R/susie_susie_utils.R
@@ -846,4 +846,17 @@ kriging_rss = function (z, R, n, r_tol = 1e-08,
   return(list(plot = p,conditional_dist = res))
 }
 
-
+# @title Utility function to display warning messages as they occur
+# @param ... warning message
+# @param style either "warning" or "hint"
+#'@importFrom crayon combine_styles
+warning_message = function(..., style=c("warning", "hint")) {
+  style = match.arg(style)
+  if (style=="warning" && getOption("warn")>=0) {
+    alert <- combine_styles("bold", "underline", "red")
+    message(alert("WARNING:"), " ", ...)
+  } else {
+    alert <- combine_styles("bold", "underline", "magenta")
+    message(alert("HINT:"), " ", ...)
+  }
+}
