ba52dadf83479381e588dcae59bf5d6866a6402f
kevinlkx <kevinlkx@gmail.com>
2024-06-13 15:54:07 -0500

    update tests to work without using RCC

--- tests/testthat/test-ctwas_compute_region_cor.R
+++ tests/testthat/test-ctwas_compute_region_cor.R
@@ -1,4 +1,4 @@
-test_that("get_region_cor works", {
+test_that("get_region_cor loads correlation matrices", {
 
   region_id <- "16_71020125_72901251"
   R_gene <- readRDS(system.file("extdata/sample_data/cor_matrix", paste0("region.", region_id, ".R_gene.RDS"), package = "ctwas"))
@@ -13,17 +13,17 @@ test_that("get_region_cor works", {
   cor_res <- get_region_cor(region_id, cor_dir = cor_dir)
   expect_equal(cor_res, precomputed_cor_res)
 
-  # test computing the correlation matrices
-  region_info <- readRDS(system.file("extdata/sample_data", "LDL_example.region_info.RDS", package = "ctwas"))
-  snp_info <- readRDS(system.file("extdata/sample_data", "LDL_example.snp_info.RDS", package = "ctwas"))
-  LD_info <- readRDS(system.file("extdata/sample_data", "LDL_example.LD_info.RDS", package = "ctwas"))
-  weights <- readRDS(system.file("extdata/sample_data", "LDL_example.preprocessed.weights.RDS", package = "ctwas"))
-  ctwas_res <- readRDS(system.file("extdata/sample_data", "LDL_example.ctwas_sumstats_res.RDS", package = "ctwas"))
-  screened_region_data <- ctwas_res$screened_region_data
-  cor_res <- get_region_cor(region_id,
-                            region_data = screened_region_data,
-                            LD_info = LD_info,
-                            snp_info = snp_info,
-                            weights = weights)
-  expect_equal(cor_res, precomputed_cor_res)
+  # # test computing the correlation matrices
+  # region_info <- readRDS(system.file("extdata/sample_data", "LDL_example.region_info.RDS", package = "ctwas"))
+  # snp_info <- readRDS(system.file("extdata/sample_data", "LDL_example.snp_info.RDS", package = "ctwas"))
+  # LD_info <- readRDS(system.file("extdata/sample_data", "LDL_example.LD_info.RDS", package = "ctwas"))
+  # weights <- readRDS(system.file("extdata/sample_data", "LDL_example.preprocessed.weights.RDS", package = "ctwas"))
+  # ctwas_res <- readRDS(system.file("extdata/sample_data", "LDL_example.ctwas_sumstats_res.RDS", package = "ctwas"))
+  # screened_region_data <- ctwas_res$screened_region_data
+  # cor_res <- get_region_cor(region_id,
+  #                           region_data = screened_region_data,
+  #                           LD_info = LD_info,
+  #                           snp_info = snp_info,
+  #                           weights = weights)
+  # expect_equal(cor_res, precomputed_cor_res)
 })
