fdb92fe81bcb4df2804198b7c0ecbce042b623c2
kevinlkx <kevinlkx@gmail.com>
2024-06-16 18:06:59 -0500

    add more tests

---
+++ tests/testthat/test-ctwas_summarize_parameters.R
@@ -0,0 +1,11 @@
+test_that("summarize_param works", {
+
+  gwas_n <- 343621
+  ctwas_res <- readRDS(system.file("extdata/sample_data", "LDL_example.ctwas_sumstats_res.RDS", package = "ctwas"))
+  param <- ctwas_res$param
+  precomputed_ctwas_parameters <- readRDS(system.file("extdata/sample_data", "LDL_example.ctwas_parameters_no_plots.RDS", package = "ctwas"))
+
+  ctwas_parameters <- summarize_param(param, gwas_n, plot = FALSE)
+  expect_equal(ctwas_parameters, precomputed_ctwas_parameters)
+
+})
