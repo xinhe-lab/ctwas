34d44de08653f5394e5fb66cbca2bbdf14f85ba0
kevinlkx <kevinlkx@gmail.com>
2025-01-07 13:59:28 -0600

    update documentations and tests

--- tests/testthat/test-ctwas_est_parameters.R
+++ tests/testthat/test-ctwas_est_parameters.R
@@ -8,8 +8,23 @@ test_that("est_param works", {
     param <- est_param(region_data,
                        niter_prefit = 3,
                        niter = 30,
-                       min_gene = 1,
                        ncore = 2)
+  })
+
+  expect_equal(param, expected_param)
+})
+
+test_that("est_param_ser works", {
+
+  ctwas_res <- readRDS(system.file("extdata/sample_data", "LDL_example.ctwas_sumstats_noLD_res.RDS", package = "ctwas"))
+  region_data <- ctwas_res$region_data
+  expected_param <- ctwas_res$param
+
+  capture.output({
+    param <- est_param_ser(region_data,
+                           niter_prefit = 3,
+                           niter = 30,
+                           ncore = 2)
   })
 
   expect_equal(param, expected_param)
