930380e7acacc95dee7ca599fab5ab85a3dc3976
kevinlkx <kevinlkx@gmail.com>
2025-06-08 17:18:18 -0500

    update tests

--- tests/testthat/test-ctwas_weights.R
+++ tests/testthat/test-ctwas_weights.R
@@ -33,15 +33,16 @@ test_that("load_weights works for FUSION weights", {
 test_that("trim_weights works", {
 
   snp_map <- readRDS(system.file("extdata/sample_data", "LDL_example.snp_map.RDS", package = "ctwas"))
+  z_snp <- readRDS(system.file("extdata/sample_data", "LDL_example.preprocessed.z_snp.RDS", package = "ctwas"))
 
   weights <- readRDS("LDL_example.preprocessed.liver.weights.RDS")
   expected_trimmed_weights <- readRDS("LDL_example.preprocessed.trimmed.liver.weights.RDS")
 
   capture.output({
-    trimmed_weights <- trim_weights(weights, snp_map, top_n_snps = 2)
+    trimmed_weights <- trim_weights(weights, snp_map, z_snp$id, top_n_snps = 2)
   })
 
-  # saveRDS(trimmed_weights, "LDL_example.preprocessed.trimmed.liver.weights.RDS")
+  saveRDS(trimmed_weights, "LDL_example.preprocessed.trimmed.liver.weights.RDS")
 
   expect_equal(trimmed_weights, expected_trimmed_weights)
 
